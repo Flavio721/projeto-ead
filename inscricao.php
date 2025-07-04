bc080febaad8825d0bc66a13c10ad7216cc98b63
Flavio721 <flavio2010sjc@gmail.com>
2025-07-04 10:23:45 -0300

    Novo sistema

--- inscricao.php
+++ inscricao.php
@@ -1,58 +1,30 @@
 <?php
-if(isset($_POST['submit'])){
-  include_once('database.php');
+include_once('database.php');
 
-  $nome = $_POST['nome'];
-  $sobrenome = $_POST['sobre'];
-  $email = $_POST['email']; 
-  $senha = $_POST['senha'];
-  $cep = $_POST['cep'];
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $nome = $_POST['nome'];
+    $sobrenome = $_POST['sobrenome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $cep = $_POST['cep'];
+    $rua = $_POST['rua'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
 
-  $result = mysqli_query($conexao, "INSERT INTO inscricao(nome, sobrenome, email, senha, cep) VALUES ('$nome', '$sobrenome', '$email', '$senha', '$cep')");
-   
-  // Criar page para inscrição concluida
+    $stmt = $conexao->prepare("INSERT INTO inscricao (nome, sobrenome, email, senha, cep, rua, bairro, cidade)VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+    $stmt->bind_param("ssssssss", $nome, $sobrenome, $email, $senha, $cep, $rua, $bairro, $cidade);
+
+    if ($stmt->execute()) {
+        echo "Inscrição realizada com sucesso!";
+        header("Location: templates/homepage.html");
+        exit();
+    } else {
+        echo "Erro ao salvar: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conexao->close();
 }
+
 ?>
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inscrição - FakeEtec</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
-    <link rel="stylesheet" href="estilos/style.css">
-  </head>
-  <body>
-    <main>
-        <div class="form">
-            <div class="voltar">
-                <a href="homepage.html">
-                <span class="material-symbols-outlined">
-                    arrow_back_ios
-                </span>
-                </a>
-            </div>
-            <form action="inscricao.php" method="post">
-                <label for="inome" class="form-label">Nome</label>
-                <input type="text" name="nome" id="inome" required class="form-control" maxlength="20" aria-label="Nome">
-
-                <label for="isobre" class="form-label">Sobrenome</label>
-                <input type="text" name="sobre" id="isobre" required class="form-control" maxlength="20" aria-label="Sobrenome">
-
-                <label for="iemail" class="form-label">Email</label>
-                <input type="email" name="email" id="iemail" required class="form-control" maxlength="50" aria-label="Email">
-
-                <label for="isenha" class="form-label">Senha</label>
-                <input type="password" name="senha" id="isenha" required class="form-control" maxlength="16" aria-label="Senha">
-
-                <label for="icep" class="form-label">CEP</label>
-                <input type="number" name="cep" id="icep" required class="form-control" minlength="9" maxlength="9" aria-label="CEP">
-
-                <input type="submit" name="submit" value="Enviar" class="btn btn-primary" style="margin-top: 10px;">
-            </form>
-        </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-  </body>
-</html>
